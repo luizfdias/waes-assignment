3d9e97f40c187853c8b24d9f488d2febc83ecbe4
Luiz Fernando Dias Rezende <luizdias.inf@gmail.com>
2019-03-20 10:15:14 -0300

    Including new tests.

---
+++ Waes.Assignment.UnitTests/Application/Services/DiffServiceTests.cs
@@ -0,0 +1,75 @@
+﻿using AutoFixture.Idioms;
+using AutoMapper;
+using FluentAssertions;
+using NSubstitute;
+using NSubstitute.ReturnsExtensions;
+using Waes.Assignment.Application.Services;
+using Waes.Assignment.Application.ViewModels;
+using Waes.Assignment.Domain.Events;
+using Waes.Assignment.Domain.Interfaces;
+using Waes.Assignment.Domain.Models;
+using Waes.Assignment.UnitTests.AutoData;
+using Xunit;
+
+namespace Waes.Assignment.UnitTests.Application.Services
+{
+    public class DiffServiceTests
+    {
+        private readonly IDiffRepository _diffRepository;
+
+        private readonly IMapper _mapper;
+
+        private readonly IMediatorHandler _mediatorHandler;
+
+        private readonly DiffService _diffService;
+
+        public DiffServiceTests()
+        {
+            _diffRepository = Substitute.For<IDiffRepository>();
+            _mapper = Substitute.For<IMapper>();
+            _mediatorHandler = Substitute.For<IMediatorHandler>();
+
+            _diffService = new DiffService(_diffRepository, _mapper, _mediatorHandler);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Constructor_GuardTests(GuardClauseAssertion guard)
+        {
+            guard.Verify(typeof(DiffService).GetConstructors());
+        }
+
+        [Theory, AutoNSubstituteData]
+        public async void Get_WhenDiffIsNotFound_ShouldRaiseDiffNotFoundEvent(string correlationId)
+        {
+            _diffRepository.GetByCorrelationId(Arg.Any<string>()).ReturnsNull();
+            
+            var result = await _diffService.Get(correlationId);
+
+            await _mediatorHandler.Received(1).RaiseEvent(Arg.Is<DiffNotFoundEvent>(d => d.CorrelationId == correlationId));
+        }
+
+        [Theory, AutoNSubstituteData]
+        public async void Get_WhenDiffIsFound_ShouldReturnDiffResponse(string correlationId, Diff diff, DiffResponse diffResponse)
+        {
+            _diffRepository.GetByCorrelationId(correlationId).Returns(diff);
+
+            _mapper.Map<DiffResponse>(diff).Returns(diffResponse);
+
+            var result = await _diffService.Get(correlationId);
+
+            result.Should().Be(diffResponse);            
+        }
+
+        [Theory, AutoNSubstituteData]
+        public async void Get_WhenDiffIsFound_ShouldNotRaiseDiffNotFoundEvent(string correlationId, Diff diff, DiffResponse diffResponse)
+        {
+            _diffRepository.GetByCorrelationId(correlationId).Returns(diff);
+
+            _mapper.Map<DiffResponse>(diff).Returns(diffResponse);
+
+            var result = await _diffService.Get(correlationId);
+
+            await _mediatorHandler.DidNotReceive().RaiseEvent(Arg.Any<DiffNotFoundEvent>());
+        }
+    }
+}
---
+++ Waes.Assignment.UnitTests/Application/Services/PayLoadServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using AutoFixture.Idioms;
+using AutoMapper;
+using NSubstitute;
+using Waes.Assignment.Application.Interfaces;
+using Waes.Assignment.Application.Services;
+using Waes.Assignment.Domain.Interfaces;
+using Waes.Assignment.UnitTests.AutoData;
+using Xunit;
+
+namespace Waes.Assignment.UnitTests.Application.Services
+{
+    public class PayLoadServiceTests
+    {
+        private readonly IMapper _mapper;
+
+        private readonly IMediatorHandler _mediatorHandler;
+
+        private readonly INotificationHandler _notificationHandler;
+
+        private readonly PayLoadService _payLoadService;
+
+        public PayLoadServiceTests()
+        {
+            _mapper = Substitute.For<IMapper>();
+            _mediatorHandler = Substitute.For<IMediatorHandler>();
+            _notificationHandler = Substitute.For<INotificationHandler>();
+
+            _payLoadService = new PayLoadService(_mediatorHandler, _mapper, _notificationHandler);
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Constructor_GuardTests(GuardClauseAssertion guard)
+        {
+            guard.Verify(typeof(PayLoadService).GetConstructors());
+        }
+
+        [Theory, AutoNSubstituteData]
+        public void Create_GuardTests(string correlationId)
+        {
+            _payLoadService.Create(correlationId, )
+        }
+    }
+}
