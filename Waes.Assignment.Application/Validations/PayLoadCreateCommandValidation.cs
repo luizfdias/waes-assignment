27050d8279114d2337761fe7af67fe88f170ba10
Luiz Fernando Dias Rezende <luizdias.inf@gmail.com>
2019-04-01 09:09:20 -0300

    Adding docker file and validation for payloadcreatecommand

---
+++ Waes.Assignment.Application/Validations/PayLoadCreateCommandValidation.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Waes.Assignment.Application.Interfaces;
+using Waes.Assignment.Domain.Commands;
+
+namespace Waes.Assignment.Application.Validations
+{
+    public class PayLoadCreateCommandValidation : AbstractValidator<PayLoadCreateCommand>
+    {
+        private readonly IPayLoadRepository _payLoadRepository;
+
+        public PayLoadCreateCommandValidation(IPayLoadRepository payLoadRepository)
+        {
+            _payLoadRepository = payLoadRepository ?? throw new ArgumentNullException(nameof(payLoadRepository));
+
+            WhenAsync(NotExistAsync, () =>
+            {
+
+            });
+        }
+
+        private async Task<bool> NotExistAsync(PayLoadCreateCommand correlationId, CancellationToken cancellationToken)
+        {
+            var payload = await _payLoadRepository.GetByCorrelationId(correlationId);
+
+            return payload == null;
+        }
+    }
+}
